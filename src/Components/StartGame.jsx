77ddb7f4c2aeb4dca4a3f13d4c60f0aad35f08bc
dhruv06122002 <dhruvitwala@gmail.com>
2024-09-24 21:46:02 +0530

    Design Update

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -4,8 +4,9 @@ const StartGame = ({ toggle }) => {
   return (
     <Container>
       <div>
-        <img src="Dices.png" />
+        <img src="Dices.png" height="442" width="550" />
       </div>
+      <br />
       <div className="content">
         <h1>Dice Game</h1>
         <Button onClick={toggle}>Play Game</Button>
@@ -20,10 +21,14 @@ const Container = styled.div`
   max-width: 1180px;
   height: 100vh;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
   align-items: center;
 
   .content {
+    text-align: center;
     h1 {
       font-size: 96px;
       white-space: nowrap;
